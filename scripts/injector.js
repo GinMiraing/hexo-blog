c627bb82c3ecfcf2a0aea091eec6dfc40c4506b2
GinMiraing <ginmiraing@outlook.com>
2023-05-03 23:18:37 +0800

    delete:删除蒙版

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,3 +1,3 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.bodyBegin.raw('default', '<div id="web-bg"></div><div id="web-mask"></div>');
+    injects.bodyBegin.raw('default', '<div id="web-bg"></div>');
 });
